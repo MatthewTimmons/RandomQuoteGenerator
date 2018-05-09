b5747345dc970f53e785732c2c658c8e23896c29
Matthew Timmons <Matthewbt@yahoo.com>
2018-05-08 22:25:55 -0500

    Finished working project
    Changed quotes
    Made it so it would display citation and year if applicable
    Added tags to quotes and printed them to display

--- js/script.js
+++ js/script.js
@@ -2,64 +2,72 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//Initialzing the quotes array
 var quotes = [
 	{
-		quote: 'Did I do that?',
-		source: 'Erkel',
-		citation: 'Full House',
-		year: 1980
+		quote: "A journey of a thousand miles must begin with a single step",
+		source: "Lao Tzu",
+		tags: ["philosophers", "nonfiction"]
 	}, 
 	{
-		quote: 'Howdy doo',
-		source: 'Flanders',
-		citation: 'The Simpsons',
-		year: 1980
+		quote: "If you can't explain it simply, you don't understand it well enough",
+		source: "Albert Einstein",
+		tags: ["famous people", "nonfiction"]
 	},
 	{
-		quote: 'Hey, hey, hey',
-		source: 'Fat Albert',
-		year: 1980
+		quote: "I think, therefore I am",
+		source: "Rene Descartes",
+		tags: ["philosophers", "nonfiction"]
 	},
 	{
-		quote: 'Are you having a laugh?',
-		source: 'Ricky Gervais',
-		citation: 'Extras',
-		year: 2005
+		quote: "The only thing I know is that I know nothing",
+		source: "Socrates",
+		tags: ["philosophers", "nonfiction"]
 	},
 	{
-		quote: 'You\'re not supposed to do that. You know you\'re not supposed to do that',
-		source: 'British Guy',
-		citation: 'Peep Show',
-		year: 2008
+		quote: "Great minds discuss ideas; average minds discuss events; small minds discuss people",
+		source: "Eleanor Roosevelt",
+		tags: ["famous people", "nonfiction"]
 	},
 	{
-		quote: 'Have you tried turning it off and on again?',
-		source: 'Maurice Moss',
-		citation: 'The IT Crowd',
-		year: 2009
+		quote: "Some are born great, some acheive greatness, and some have greatness thrust upon them",
+		source: "William Shakespeare",
+		tags: ["famous people", "nonfiction"]
 	},
 	{
-		quote: 'What if I can\'t forget you?',
-		source: 'Pierce the Veil',
-		citation: 'Caraphernelia',
+		quote: "I am the one who knocks",
+		source: "Walter White",
+		tags: ["television", "fiction"],
+		citation: "Breaking Bad",
 		year: 2011
 	}
 ];
 
 
-
+//Creating functions
 function getRandomQuote() {
-	return quotes[Math.floor(Math.random * quotes.length)];
+	var id = Math.floor(Math.random() * quotes.length);
+	return quotes[id];
 }
 
 function printQuote() {
+	//Getting quote
 	var quote = getRandomQuote();
-	var html = 
-	`<p class="quote"> ${quote.quote} </p>
-	<p class="source"> ${quote.source} </p>
-		<span class="citation"> ${quote.citation} </span>
-		<span class="year"> ${quote.year} </span>
-	</p>`
 
+	//Interpolating quote and source
+	var html = `<p class="quote"> ${quote.quote} </p><p class="source"> ${quote.source}`;
+
+	//Interpolating citation and year if applicable
+	if (quote.citation) {
+		html += `<span class="citation"> ${quote.citation} </span>`
+	}
+	if (quote.year) {
+		html += `<span class="year"> ${quote.year} </span>`
+	}
+
+	//Closing out paragraph tag and printing each quotes tags
+	html += `</p><p class="tags"> Tags: ${quote.tags} </p>`;
+
+	//Inserting html into webpage
 	document.getElementById('quote-box').innerHTML = html;
 }
